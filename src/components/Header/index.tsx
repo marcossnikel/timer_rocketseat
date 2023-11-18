e5c5a4222e8cba9f1e8fcd10d0ed373af08029f2
marcossnikel <marcosnikeldev@gmail.com>
2023-11-18 00:20:20 -0300

    header and learning about routes react-router-dom

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import { HeaderContainer } from './style'
+import logoIgnite from '../../assets/IgniteLogo.svg'
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="History">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
