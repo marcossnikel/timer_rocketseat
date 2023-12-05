e8d84e083602acb94ff57a4484a17980c8f558cd
marcossnikel <marcosnikeldev@gmail.com>
2023-12-05 19:32:08 -0300

    Add cycles reducer with actions and state

---
+++ src/reducers/cycles/reducer.ts
@@ -0,0 +1,55 @@
+import { ActionTypes } from './actions'
+import { produce } from 'immer'
+export type Cycle = {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+  interruptedDate?: Date
+  finishedDate?: Date
+}
+
+type CyclesState = {
+  cycles: Cycle[]
+  activeCycleId: string | null
+}
+
+export const cyclesReducer = (state: CyclesState, action: any) => {
+  switch (action.type) {
+    case ActionTypes.ADD_NEW_CYCLE:
+      return produce(state, (draftState) => {
+        draftState.cycles.push(action.payload.newCycle)
+        draftState.activeCycleId = action.payload.newCycle.id
+      })
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
+      const currentCycleIndex = state.cycles.findIndex(
+        (cycle) => cycle.id === state.activeCycleId,
+      )
+
+      if (currentCycleIndex < 0) {
+        return state
+      }
+
+      return produce(state, (draftState) => {
+        draftState.activeCycleId = null
+        draftState.cycles[currentCycleIndex].interruptedDate = new Date()
+      })
+    }
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
+      const currentCycleIndex = state.cycles.findIndex(
+        (cycle) => cycle.id === state.activeCycleId,
+      )
+
+      if (currentCycleIndex < 0) {
+        return state
+      }
+
+      return produce(state, (draftState) => {
+        draftState.activeCycleId = null
+        draftState.cycles[currentCycleIndex].finishedDate = new Date()
+      })
+    }
+    default:
+      return state
+  }
+}
